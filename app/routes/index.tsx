644af11f595f5b742cb79da07205d076640206df
Bangun <hibangun@gmail.com>
2022-02-20 01:33:28 +0700

    Add sample beranda components

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,22 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="grid grid-cols-4 gap-6">
+      <div className="bg-white drop-shadow-md text-center rounded-lg overflow-hidden">
+        <p className="bg-slate-300 p-2">Total transaksi</p>
+        <h1 className="text-5xl p-8">1,4 M</h1>
+      </div>
+      <div className="bg-white drop-shadow-md text-center rounded-lg overflow-hidden">
+        <p className="bg-slate-300 p-2">Total user</p>
+        <h1 className="text-5xl p-8">600 k</h1>
+      </div>
+      <div className="bg-white drop-shadow-md text-center rounded-lg overflow-hidden">
+        <p className="bg-slate-300 p-2">Total pemesanan</p>
+        <h1 className="text-5xl p-8">40,000</h1>
+      </div>
+      <div className="bg-white drop-shadow-md text-center rounded-lg overflow-hidden">
+        <p className="bg-slate-300 p-2">Total driver</p>
+        <h1 className="text-5xl p-8">1.2 k</h1>
+      </div>
     </div>
-  );
+  )
 }
