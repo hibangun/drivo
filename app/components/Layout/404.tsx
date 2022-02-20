d44cb305504c50815e9e7f666684a3545bb6f868
Bangun <hibangun@gmail.com>
2022-02-21 00:54:50 +0700

    Update 404 page function

--- app/components/Layout/404.tsx
+++ app/components/Layout/404.tsx
@@ -1,6 +1,7 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { Link } from 'remix'
 import Header from './Header'
+import Sidebar from './Sidebar'
 
 type Layout404Props = {
   children?: never
@@ -14,24 +15,34 @@ const Layout404: FC<Layout404Props> = function Layout404({
   backlinkTo,
   backlinkText
 }) {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen)
+  }
   return (
     <div className="flex flex-col h-screen">
-      <Header />
-      <div className="min-h-full flex item-center justify-center py-12 px-4 sm:px-6lg:px-n">
-        <div className="max-w-d w-full space-y-8">
-          <div className="lg:text-center">
-            <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
-              404
-            </h2>
-            <p className="mt-2 text-3xl landing-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-              {title}
-            </p>
-            <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-              <Link to={backlinkTo}>{backlinkText}</Link>
-            </p>
+      <Header toggleMobileMenu={toggleMobileMenu} />
+      <main className="flex flex-row">
+        <Sidebar isMobileMenuOpen={isMobileMenuOpen} />
+        <section className="bg-slate-50 w-full md:w-9/12 p-6 min-h-screen">
+          <div className="min-h-full flex item-center justify-center py-12 px-4 sm:px-6lg:px-n">
+            <div className="max-w-d w-full space-y-8">
+              <div className="lg:text-center">
+                <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
+                  404
+                </h2>
+                <p className="mt-2 text-3xl landing-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                  {title}
+                </p>
+                <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+                  <Link to={backlinkTo}>{backlinkText}</Link>
+                </p>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </section>
+      </main>
     </div>
   )
 }
